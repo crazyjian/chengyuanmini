bcef50cb98c9eb45feaa71e28894fcdd67863440
DMcoders <hengzhang64@gmail.com>
2019-07-20 14:11:26 +0800

    Initial Commit

---
+++ pages/logs/logs.js
@@ -0,0 +1,12 @@
+//logs.js
+const util = require('../../utils/util.js')
+const app = getApp()
+Page({
+  data: {
+    backImg: app.globalData.backImg,
+    placeholder: '请扫描工厂二维码'
+  },
+  onLoad: function () {
+   
+  }
+})
